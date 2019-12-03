02ac926e754e010285dcf02027c2d0af8e850f3a
JulianAndr3s <juanbo97@hotmail.com>
2019-12-03 07:42:15 -0500

    Creación del modelo Cliente
    Se creo el modelo cliente en el dominio con su respectivo DTO.

---
+++ backendTiendaVideojuegos/src/main/java/com/ceiba/adn/tiendavideojuegos/dominio/modelo/dto/ClienteDTO.java
@@ -0,0 +1,84 @@
+package com.ceiba.adn.tiendavideojuegos.dominio.modelo.dto;
+
+public class ClienteDTO {
+	
+	private Long idCliente;
+	private String nombre;
+	private String apellido;
+	private String telefono;
+	private String correo;
+	private String cedula;
+	private Boolean estado;
+	
+	public ClienteDTO() {
+		
+	}
+
+	public ClienteDTO(Long idCliente, String nombre, String apellido, String telefono, String correo, String cedula,
+			Boolean estado) {
+		super();
+		this.idCliente = idCliente;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.telefono = telefono;
+		this.correo = correo;
+		this.cedula = cedula;
+		this.estado = estado;
+	}
+
+	public Long getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(Long idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public Boolean getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Boolean estado) {
+		this.estado = estado;
+	}
+}
