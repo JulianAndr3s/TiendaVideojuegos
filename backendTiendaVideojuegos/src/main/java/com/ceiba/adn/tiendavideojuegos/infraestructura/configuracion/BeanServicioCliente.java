f051dccdbda653f206b7899ad182a48f6afc8de8
JulianAndr3s <juanbo97@hotmail.com>
2019-12-03 15:23:30 -0500

    Ajuste del Bean para Listar Cliente

--- backendTiendaVideojuegos/src/main/java/com/ceiba/adn/tiendavideojuegos/infraestructura/configuracion/BeanServicioCliente.java
+++ backendTiendaVideojuegos/src/main/java/com/ceiba/adn/tiendavideojuegos/infraestructura/configuracion/BeanServicioCliente.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.ceiba.adn.tiendavideojuegos.dominio.puerto.repositorio.RepositorioCliente;
 import com.ceiba.adn.tiendavideojuegos.dominio.servicios.cliente.ServicioCrearCliente;
+import com.ceiba.adn.tiendavideojuegos.dominio.servicios.cliente.ServicioListarClientes;
 
 @Configuration
 public class BeanServicioCliente {
@@ -12,6 +13,11 @@ public class BeanServicioCliente {
 	@Bean
 	public ServicioCrearCliente servicioCrearUsuario(RepositorioCliente repositorioCliente) {
 		return new ServicioCrearCliente(repositorioCliente);
+	}
+	
+	@Bean
+	public ServicioListarClientes servicioListarUsuario(RepositorioCliente repositorioCliente) {
+		return new ServicioListarClientes(repositorioCliente);
 	}
 
 }
