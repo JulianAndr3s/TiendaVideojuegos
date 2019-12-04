893646cf10d35f5ed7e9599253e381ab86b17f60
JulianAndr3s <juanbo97@hotmail.com>
2019-12-04 09:31:45 -0500

    Creación del modelo Videojuego
    Se creo el modelo videojuego en el dominio con su respectivo DTO.

---
+++ backendTiendaVideojuegos/src/main/java/com/ceiba/adn/tiendavideojuegos/dominio/modelo/Videojuego.java
@@ -0,0 +1,72 @@
+package com.ceiba.adn.tiendavideojuegos.dominio.modelo;
+
+import java.util.Date;
+
+public class Videojuego {
+	
+	private Long idVideojuego;
+	private String nombre;
+	private String genero;
+	private Date fechaLanzamiento;
+	private Integer precio;
+	private String autor;
+	
+	public Videojuego(Long idVideojuego, String nombre, String genero, Date fechaLanzamiento,
+			Integer precio, String autor) {
+		this.idVideojuego = idVideojuego;
+		this.nombre = nombre;
+		this.genero = genero;
+		this.fechaLanzamiento = fechaLanzamiento;
+		this.precio = precio;
+		this.autor = autor;
+	}
+
+	public Long getIdVideojuego() {
+		return idVideojuego;
+	}
+
+	public void setIdVideojuego(Long idVideojuego) {
+		this.idVideojuego = idVideojuego;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public Date getFechaLanzamiento() {
+		return fechaLanzamiento;
+	}
+
+	public void setFechaLanzamiento(Date fechaLanzamiento) {
+		this.fechaLanzamiento = fechaLanzamiento;
+	}
+
+	public Integer getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(Integer precio) {
+		this.precio = precio;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	
+}
