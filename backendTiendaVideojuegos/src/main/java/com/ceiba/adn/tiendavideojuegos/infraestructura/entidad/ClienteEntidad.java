a83b8ebc9f7d2de0d90d48854a4e874f86d8ad92
JulianAndr3s <juanbo97@hotmail.com>
2019-12-05 16:09:55 -0500

    Eliminar @Getter y @Setter
    Se elimina los Get y Set de Entidad Cliente.

--- backendTiendaVideojuegos/src/main/java/com/ceiba/adn/tiendavideojuegos/infraestructura/entidad/ClienteEntidad.java
+++ backendTiendaVideojuegos/src/main/java/com/ceiba/adn/tiendavideojuegos/infraestructura/entidad/ClienteEntidad.java
@@ -7,11 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 @Entity
 @Table(name = "cliente")
 public class ClienteEntidad {
