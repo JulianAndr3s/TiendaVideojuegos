fff94fc3cbafe527657a0586cc2379aec9c169fd
JulianAndr3s <juanbo97@hotmail.com>
2019-12-06 13:54:31 -0500

    Test Integración al Controlador Cliente
    Se testea el controlador del Cliente con pruebas de integración usando MockMvc

--- backendTiendaVideojuegos/src/test/java/com/ceiba/adn/tiendavideojuegos/infraestructura/integracion/controlador/ControladorClienteTest.java
+++ backendTiendaVideojuegos/src/test/java/com/ceiba/adn/tiendavideojuegos/infraestructura/integracion/controlador/ControladorClienteTest.java
@@ -4,9 +4,10 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import javax.transaction.Transactional;
+import java.util.ArrayList;
 
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
@@ -15,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -22,13 +24,16 @@ import org.springframework.web.context.WebApplicationContext;
 
 import com.ceiba.adn.tiendavideojuegos.BackendTiendaVideojuegosApplication;
 import com.ceiba.adn.tiendavideojuegos.aplicacion.comando.ComandoCliente;
+import com.ceiba.adn.tiendavideojuegos.dominio.modelo.Cliente;
+import com.ceiba.adn.tiendavideojuegos.infraestructura.adaptador.repositorio.RepositorioClienteImpl;
+import com.ceiba.adn.tiendavideojuegos.infraestructura.repositoriojpa.RepositorioClienteJpa;
 import com.ceiba.adn.tiendavideojuegos.testdatabuilder.aplicacion.comando.ComandoClienteTestDataBuilder;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = BackendTiendaVideojuegosApplication.class)
 @AutoConfigureMockMvc
-@Transactional
+@TestPropertySource("/test.properties")
 class ControladorClienteTest {
 
 	private static final Long ID_PARA_TEST = 1L;
@@ -42,71 +47,65 @@ class ControladorClienteTest {
 	@Autowired
 	private WebApplicationContext webApplicationContext;
 	
+	@Autowired
+	private RepositorioClienteJpa repositorioClienteJpa;
+	
 	@Before
     public void setup() throws Exception {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
     }
 	
 	@Test
-	void flujoControladorTest() throws Exception{
-		
+	void crearClienteTest() throws Exception {
 		ComandoCliente comandoCliente = new ComandoClienteTestDataBuilder().build();
-		
-		try {		
-		mockMvc.perform(delete("/cliente/".concat((ID_PARA_TEST).toString()).concat("/eliminar")).contentType
-				(MediaType.TEXT_PLAIN).content(objectMapper.writeValueAsString
-						(comandoCliente.getIdCliente()))).andExpect(status().isOk());
-			
-		mockMvc.perform(post("/cliente/crear").contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(comandoCliente))).andExpect(status().isOk());
-		
-		mockMvc.perform(put("/cliente/".concat((ID_PARA_TEST).toString()).concat("/actualizar")).contentType
-				(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString
-						(comandoCliente))).andExpect(status().isOk());
-		
-		mockMvc.perform(get("/cliente/listar")).andExpect(status().isOk());
-		
-		} catch(Exception excepcionTest){
-			System.out.println(excepcionTest.getCause().getMessage());
-		}
+		mockMvc.perform(post("/cliente/crear")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(comandoCliente)))
+				.andDo(print())
+				.andExpect(status().isOk());
 	}
 	
 	@Test
-	void excepcionActualizarClienteTest() throws Exception {
-		ComandoCliente comandoCliente = new ComandoClienteTestDataBuilder().build();
+	void eliminarClienteTest() throws Exception {
+		RepositorioClienteImpl repositorioClienteImpl = new RepositorioClienteImpl(repositorioClienteJpa);
+		Cliente cliente = new Cliente(1L,"J","U","L","I","A","N");
+		repositorioClienteImpl.crearCliente(cliente);
+		mockMvc.perform(delete("/cliente/".concat((ID_PARA_TEST).toString()).concat("/eliminar"))
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(cliente)))
+				.andDo(print())
+				.andExpect(status().isOk());
+	}
+	
+	@Test
+	void actualizarClienteTest() throws Exception {
+		RepositorioClienteImpl repositorioClienteImpl = new RepositorioClienteImpl(repositorioClienteJpa);
+		Cliente cliente = new Cliente(1L,"J","U","L","I","A","N");
+		repositorioClienteImpl.crearCliente(cliente);
+		cliente.setNombre("JULIAN");
+		mockMvc.perform(put("/cliente/".concat((ID_PARA_TEST).toString()).concat("/actualizar"))
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(cliente)))
+				.andDo(print())
+				.andExpect(status().isOk());	
+	}
+	
+	@Test
+	void listarClienteTest() throws Exception {
+		RepositorioClienteImpl repositorioClienteImpl = new RepositorioClienteImpl(repositorioClienteJpa);
+		Cliente cliente = new Cliente(1L,"J","U","L","I","A","N");
+		Cliente cliente2 = new Cliente(2L,"J","U","L","I","A","N");
+		repositorioClienteImpl.crearCliente(cliente);
+		repositorioClienteImpl.crearCliente(cliente2);
 		
-		try {	
-			mockMvc.perform(put("/cliente/".concat((ID_PARA_TEST).toString()).concat("/actualizar")).contentType
-					(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString
-							(comandoCliente))).andExpect(status().isInternalServerError());
-		} catch(Exception excepcionTest){
-			System.out.println(excepcionTest.getCause().getMessage());
-		}
-	}
-	
-	@Test
-	void excepcionEliminarClienteTest() throws Exception {
-		ComandoCliente comandoCliente = new ComandoClienteTestDataBuilder().build();
-		try {
-			mockMvc.perform(delete("/cliente/".concat((ID_PARA_TEST).toString()).concat("/eliminar")).contentType
-					(MediaType.TEXT_PLAIN).content(objectMapper.writeValueAsString
-							(comandoCliente.getIdCliente()))).andExpect(status().isInternalServerError());
-		} catch(Exception excepcionTest) {
-			System.out.println(excepcionTest.getCause().getMessage());
-		}
-	}
-	
-	@Test
-	void excepcionCrearClienteTest() throws Exception {
-		ComandoCliente comandoCliente = new ComandoClienteTestDataBuilder().build();
-		mockMvc.perform(post("/cliente/crear").contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(comandoCliente))).andExpect(status().isOk());
-		try {	
-			mockMvc.perform(post("/cliente/crear").contentType(MediaType.APPLICATION_JSON)
-					.content(objectMapper.writeValueAsString
-							(comandoCliente))).andExpect(status().isInternalServerError());
-		} catch(Exception excepcionTest){
-			System.out.println(excepcionTest.getCause().getMessage());
-		}
+		ArrayList<Cliente> clientes = new ArrayList<>();
+		clientes.add(cliente);
+		clientes.add(cliente2);
+		
+		mockMvc.perform(get("/cliente/listar")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(clientes)))
+				.andDo(print())
+				.andExpect(status().isOk());
 	}
 }
