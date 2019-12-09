1135628401b09ce8c6cbfd9921b22153561c810a
JulianAndr3s <juanbo97@hotmail.com>
2019-12-09 09:46:22 -0500

    Se agrega propiedad al Id

--- backendTiendaVideojuegos/src/main/java/com/ceiba/adn/tiendavideojuegos/infraestructura/entidad/ClienteEntidad.java
+++ backendTiendaVideojuegos/src/main/java/com/ceiba/adn/tiendavideojuegos/infraestructura/entidad/ClienteEntidad.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 public class ClienteEntidad {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id_cliente")
 	private Long idCliente;
 	@Column(name = "nombre")
--- backendTiendaVideojuegos/src/main/java/com/ceiba/adn/tiendavideojuegos/infraestructura/entidad/VideojuegoEntidad.java
+++ backendTiendaVideojuegos/src/main/java/com/ceiba/adn/tiendavideojuegos/infraestructura/entidad/VideojuegoEntidad.java
@@ -14,7 +14,7 @@ import javax.persistence.Table;
 public class VideojuegoEntidad {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id_videojuego")
 	private Long idVideojuego;
 	@Column(name = "nombre")
