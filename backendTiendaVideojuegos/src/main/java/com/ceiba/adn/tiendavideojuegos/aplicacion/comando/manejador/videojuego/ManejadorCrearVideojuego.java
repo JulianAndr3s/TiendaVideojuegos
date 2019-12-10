4a4b1fa13d1af44233726781959f965691fdb782
JulianAndr3s <juanbo97@hotmail.com>
2019-12-10 10:46:53 -0500

    Validar lógica de negocio

--- backendTiendaVideojuegos/src/main/java/com/ceiba/adn/tiendavideojuegos/aplicacion/comando/manejador/videojuego/ManejadorCrearVideojuego.java
+++ backendTiendaVideojuegos/src/main/java/com/ceiba/adn/tiendavideojuegos/aplicacion/comando/manejador/videojuego/ManejadorCrearVideojuego.java
@@ -6,21 +6,26 @@ import com.ceiba.adn.tiendavideojuegos.aplicacion.comando.ComandoVideojuego;
 import com.ceiba.adn.tiendavideojuegos.aplicacion.comando.fabrica.FabricaVideojuego;
 import com.ceiba.adn.tiendavideojuegos.dominio.modelo.Videojuego;
 import com.ceiba.adn.tiendavideojuegos.dominio.servicios.videojuego.ServicioCrearVideojuego;
+import com.ceiba.adn.tiendavideojuegos.dominio.validar.ValidarLogicaNegocio;
 
 @Component
 public class ManejadorCrearVideojuego {
 	
 	private final ServicioCrearVideojuego servicioCrearVideojuego;
 	private final FabricaVideojuego fabricaVideojuego;
+	private final ValidarLogicaNegocio validarLogica = new ValidarLogicaNegocio();
 	
 	public ManejadorCrearVideojuego(ServicioCrearVideojuego servicioCrearVideojuego,
 			FabricaVideojuego fabricaVideojuego) {
 		this.servicioCrearVideojuego = servicioCrearVideojuego;
-		this.fabricaVideojuego = fabricaVideojuego;		
+		this.fabricaVideojuego = fabricaVideojuego;
 	}
 	
 	public void ejecutar(ComandoVideojuego comandoVideojuego) {
 		Videojuego videojuego = this.fabricaVideojuego.crear(comandoVideojuego);
+		validarLogica.contieneCaracteresEspeciales(videojuego.getNombre());
+		videojuego.setPrecio(validarLogica.descuentoDependiendoPrecio(videojuego.getPrecio()));
+		validarLogica.fechaEsValida(videojuego.getFechaLanzamiento());
 		this.servicioCrearVideojuego.ejecutar(videojuego);
 	}
 
