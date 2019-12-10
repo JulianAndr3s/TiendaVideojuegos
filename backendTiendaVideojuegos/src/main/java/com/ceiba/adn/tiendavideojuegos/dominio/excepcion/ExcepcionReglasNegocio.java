379c91f7f529b452fb86e640552b07da23344978
JulianAndr3s <juanbo97@hotmail.com>
2019-12-10 10:46:20 -0500

    Se implementa la lógica de negocio

--- backendTiendaVideojuegos/src/main/java/com/ceiba/adn/tiendavideojuegos/dominio/excepcion/ExcepcionReglasNegocio.java
+++ backendTiendaVideojuegos/src/main/java/com/ceiba/adn/tiendavideojuegos/dominio/excepcion/ExcepcionReglasNegocio.java
@@ -4,6 +4,9 @@ public class ExcepcionReglasNegocio extends RuntimeException {
 	
 	private static final long serialVersionUID = 1L;
 
+	public static final String CONTIENE_CARACTERES_ESPECIALES = "El nombre del videojuego contiene caracteres especiales.";
+	public static final String NO_AGREGAR_DOMINGOS = "No es posible agregar videojuegos el domingo.";
+	
     public ExcepcionReglasNegocio(String mensaje){
         super(mensaje);
     }
--- backendTiendaVideojuegos/src/main/java/com/ceiba/adn/tiendavideojuegos/dominio/validar/ValidarLogicaNegocio.java
+++ backendTiendaVideojuegos/src/main/java/com/ceiba/adn/tiendavideojuegos/dominio/validar/ValidarLogicaNegocio.java
@@ -1,7 +1,51 @@
 package com.ceiba.adn.tiendavideojuegos.dominio.validar;
 
+import java.time.LocalDate;
+
+import com.ceiba.adn.tiendavideojuegos.dominio.excepcion.ExcepcionReglasNegocio;
+
 public class ValidarLogicaNegocio {
 	
-	int pruebaValidar;
+	public static final Integer PORCENTAJE_DESCUENTO_SI_ES_MAYOR = 85;
+	public static final Integer PORCENTAJE_DESCUENTO_SI_ES_MENOR = 95;
+	public static final String DIA_QUE_NO_HAY_SERVICIO = "SUNDAY";
+	
+	public int cantidadCaracteresEspeciales(String nombreVideojuego) {
+		int contadorCaracteres = 0;
+    	nombreVideojuego = nombreVideojuego.toLowerCase();
+    	for(int i = 0; i < nombreVideojuego.length(); i++) {
+    		if ((nombreVideojuego.charAt(i) == '*') || (nombreVideojuego.charAt(i) == '/') || 
+    				(nombreVideojuego.charAt(i) == '+') || (nombreVideojuego.charAt(i) == '#') || 
+    					(nombreVideojuego.charAt(i) == '%') || (nombreVideojuego.charAt(i) == '$') ||
+    						(nombreVideojuego.charAt(i) == '@') || (nombreVideojuego.charAt(i) == '&')) {
+    			contadorCaracteres ++;
+    		}
+    	}
+    	return contadorCaracteres;
+	}
+	
+	public void contieneCaracteresEspeciales(String nombreVideojuego) {
+		int contieneCaracteres = cantidadCaracteresEspeciales(nombreVideojuego);
+		if (contieneCaracteres != 0) {
+			throw new ExcepcionReglasNegocio(ExcepcionReglasNegocio.CONTIENE_CARACTERES_ESPECIALES);
+		}
+	}
+	
+	public Integer descuentoDependiendoPrecio(Integer precio) {
+		if (precio > 250000) {
+			return (precio * PORCENTAJE_DESCUENTO_SI_ES_MAYOR) / 100;
+		}
+		else {
+			return (precio * PORCENTAJE_DESCUENTO_SI_ES_MENOR) / 100;
+		}
+	}
+	
+	public void fechaEsValida(LocalDate fechaLanzamiento) {
+		 String diaSemana = fechaLanzamiento.getDayOfWeek().toString();
+		 if (diaSemana.equals(DIA_QUE_NO_HAY_SERVICIO)) {
+			 throw new ExcepcionReglasNegocio(ExcepcionReglasNegocio.NO_AGREGAR_DOMINGOS);
+		 } 
+	}
+	
 	
 }
