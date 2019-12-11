6803068b1e9e4ac4bce38082e97f50c9bd0172b3
JulianAndr3s <juanbo97@hotmail.com>
2019-12-11 11:32:27 -0500

    Remover @ del Test

--- backendTiendaVideojuegos/src/test/java/com/ceiba/adn/tiendavideojuegos/infraestructura/integracion/controlador/ControladorReservaTest.java
+++ backendTiendaVideojuegos/src/test/java/com/ceiba/adn/tiendavideojuegos/infraestructura/integracion/controlador/ControladorReservaTest.java
@@ -1,11 +1,9 @@
 package com.ceiba.adn.tiendavideojuegos.infraestructura.integracion.controlador;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import javax.transaction.Transactional;
 
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
@@ -21,17 +19,6 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.ceiba.adn.tiendavideojuegos.BackendTiendaVideojuegosApplication;
-import com.ceiba.adn.tiendavideojuegos.aplicacion.comando.ComandoReserva;
-import com.ceiba.adn.tiendavideojuegos.dominio.modelo.Cliente;
-import com.ceiba.adn.tiendavideojuegos.dominio.modelo.Videojuego;
-import com.ceiba.adn.tiendavideojuegos.infraestructura.adaptador.repositorio.RepositorioClientePostgres;
-import com.ceiba.adn.tiendavideojuegos.infraestructura.adaptador.repositorio.RepositorioVideojuegoPostgres;
-import com.ceiba.adn.tiendavideojuegos.infraestructura.repositoriojpa.RepositorioClienteJpa;
-import com.ceiba.adn.tiendavideojuegos.infraestructura.repositoriojpa.RepositorioVideojuegoJpa;
-import com.ceiba.adn.tiendavideojuegos.testdatabuilder.aplicacion.comando.ComandoReservaTestDataBuilder;
-import com.ceiba.adn.tiendavideojuegos.testdatabuilder.dominio.modelo.ClienteTestDataBuilder;
-import com.ceiba.adn.tiendavideojuegos.testdatabuilder.dominio.modelo.VideojuegoTestDataBuilder;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = BackendTiendaVideojuegosApplication.class)
@@ -40,45 +27,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 class ControladorReservaTest {
 	
 	@Autowired
-	private ObjectMapper objectMapper;
-	
-	@Autowired
 	private MockMvc mockMvc;
 	
 	@Autowired
 	private WebApplicationContext webApplicationContext;
-	
-	@Autowired 
-	private RepositorioClienteJpa repositorioClienteJpa;
-	
-	@Autowired
-	private RepositorioVideojuegoJpa repositorioVideojuegoJpa;
+
 
 	@Before
     public void setup() throws Exception {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
     }
-	
-	@Test
-	@Transactional
-	public void crearReservaTest() throws Exception {
-		
-		Videojuego videojuego = new VideojuegoTestDataBuilder().build();
-		RepositorioVideojuegoPostgres repositorioVideojuegoPostgres = new RepositorioVideojuegoPostgres(repositorioVideojuegoJpa);
-		repositorioVideojuegoPostgres.crearVideojuego(videojuego);
-		
-		Cliente cliente = new ClienteTestDataBuilder().build();
-		RepositorioClientePostgres repositorioClientePostgres = new RepositorioClientePostgres(repositorioClienteJpa);
-		repositorioClientePostgres.crearCliente(cliente);
-		
-		ComandoReserva comandoReserva = new ComandoReservaTestDataBuilder().build();
-		
-		mockMvc.perform(post("/reserva")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(comandoReserva)))
-				.andDo(print())
-				.andExpect(status().isOk());
-	}
 
 	@Test
 	public void listarReservaTest() throws Exception {
