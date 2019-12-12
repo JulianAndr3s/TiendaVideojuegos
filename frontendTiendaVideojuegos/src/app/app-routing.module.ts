9e2282b4f5ee7673d34f0316819ede0ae05053cd
JulianAndr3s <juanbo97@hotmail.com>
2019-12-12 14:10:51 -0500

    Se hace todo lo visual del FrontEnd

--- frontendTiendaVideojuegos/src/app/app-routing.module.ts
+++ frontendTiendaVideojuegos/src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './feature/home/home.component';
+import { ListarClienteComponent } from './feature/cliente/componentes/listar-cliente/listar-cliente.component';
+import { ListarVideojuegoComponent } from './feature/videojuego/componentes/listar-videojuego/listar-videojuego.component';
+import { ListarReservaComponent } from './feature/reserva/componentes/listar-reserva/listar-reserva.component';
+import { CrearClienteComponent } from './feature/cliente/componentes/crear-cliente/crear-cliente.component';
+import { CrearReservaComponent } from './feature/reserva/componentes/crear-reserva/crear-reserva.component';
+import { CrearVideojuegoComponent } from './feature/videojuego/componentes/crear-videojuego/crear-videojuego.component';
 
+const routes: Routes = [
+  {path: 'crear-cliente', component: CrearClienteComponent},
+  {path: 'crear-reserva', component: CrearReservaComponent},
+  {path: 'crear-videojuego', component: CrearVideojuegoComponent},
+  {path: 'clientes', component: ListarClienteComponent},
+  {path: 'videojuegos', component: ListarVideojuegoComponent},
+  {path: 'reservas', component: ListarReservaComponent},
+  {path: 'principal', component: HomeComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'principal'}
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontendTiendaVideojuegos/src/app/app.module.ts
+++ frontendTiendaVideojuegos/src/app/app.module.ts
@@ -7,16 +7,11 @@ import { HomeComponent } from './feature/home/home.component';
 import { CrearVideojuegoComponent } from './feature/videojuego/componentes/crear-videojuego/crear-videojuego.component';
 import { CabeceraComponent } from './shared/componentes/cabecera/cabecera.component';
 import { PiePaginaComponent } from './shared/componentes/pie-pagina/pie-pagina.component';
-import { ActualizarVideojuegoComponent } from './feature/videojuego/componentes/actualizar-videojuego/actualizar-videojuego.component';
 import { ListarVideojuegoComponent } from './feature/videojuego/componentes/listar-videojuego/listar-videojuego.component';
-import { EliminarVideojuegoComponent } from './feature/videojuego/componentes/eliminar-videojuego/eliminar-videojuego.component';
 import { CrearClienteComponent } from './feature/cliente/componentes/crear-cliente/crear-cliente.component';
 import { ListarClienteComponent } from './feature/cliente/componentes/listar-cliente/listar-cliente.component';
-import { EliminarClienteComponent } from './feature/cliente/componentes/eliminar-cliente/eliminar-cliente.component';
-import { ActualizarClienteComponent } from './feature/cliente/componentes/actualizar-cliente/actualizar-cliente.component';
 import { CrearReservaComponent } from './feature/reserva/componentes/crear-reserva/crear-reserva.component';
 import { ListarReservaComponent } from './feature/reserva/componentes/listar-reserva/listar-reserva.component';
-import { EliminarReservaComponent } from './feature/reserva/componentes/eliminar-reserva/eliminar-reserva.component';
 
 @NgModule({
   declarations: [
@@ -25,16 +20,11 @@ import { EliminarReservaComponent } from './feature/reserva/componentes/eliminar
     CrearVideojuegoComponent,
     CabeceraComponent,
     PiePaginaComponent,
-    ActualizarVideojuegoComponent,
     ListarVideojuegoComponent,
-    EliminarVideojuegoComponent,
     CrearClienteComponent,
     ListarClienteComponent,
-    EliminarClienteComponent,
-    ActualizarClienteComponent,
     CrearReservaComponent,
     ListarReservaComponent,
-    EliminarReservaComponent
   ],
   imports: [
     BrowserModule,
