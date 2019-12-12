b5bafd4cb7f10c3abf46f8f6657dc4d51ed3b2c9
JulianAndr3s <juanbo97@hotmail.com>
2019-12-12 14:21:56 -0500

    CorsFilter

--- backendTiendaVideojuegos/src/main/java/com/ceiba/adn/tiendavideojuegos/infraestructura/configuracion/CorsFilter.java
+++ backendTiendaVideojuegos/src/main/java/com/ceiba/adn/tiendavideojuegos/infraestructura/configuracion/CorsFilter.java
@@ -2,7 +2,6 @@ package com.ceiba.adn.tiendavideojuegos.infraestructura.configuracion;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
