514ba85be66af683df174f3c6c4bc5419f618ce6
JulianAndr3s <juanbo97@hotmail.com>
2019-12-13 11:19:43 -0500

    Implementación pruebas funcionales

--- frontendTiendaVideojuegos/e2e/src/app.e2e-spec.ts
+++ frontendTiendaVideojuegos/e2e/src/app.e2e-spec.ts
@@ -1,23 +1,80 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, logging, element, by } from 'protractor';
 
-describe('workspace-project App', () => {
-  let page: AppPage;
-
-  beforeEach(() => {
-    page = new AppPage();
+describe('Prueba inicio', function() {
+  it('Prueba inicio', function() {
+    browser.get('http://localhost:4200/');
+    expect(browser.getTitle()).toEqual('FrontendTiendaVideojuegos');
+    browser.sleep(1000);
   });
+});
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('frontendTiendaVideojuegos app is running!');
+describe('insertarCliente', function() {
+  it('insertarCliente', function() {
+    browser.get('http://localhost:4200/crear-cliente');
+    browser.sleep(3000);
+
+    element(by.name('nombre')).sendKeys('Julian');
+    element(by.name('apellido')).sendKeys('Botero');
+    element(by.name('telefono')).sendKeys('5666804');
+    element(by.name('correo')).sendKeys('julian@hotmail.com');
+    element(by.name('cedula')).sendKeys('1036402404');
+    browser.sleep(2000);
+    element(by.name('estado')).sendKeys('Inactivo');
+    browser.sleep(2000);
+    element(by.id('registrar')).click();
   });
+});
 
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
+describe('listarCliente', function() {
+  it('listarCliente', function() {
+    browser.get('http://localhost:4200/clientes');
+    expect(browser.getTitle()).toEqual('FrontendTiendaVideojuegos');
+    browser.sleep(3000);
+  });
+});
+
+describe('insertarVideojuego', function() {
+  it('insertarVideojuego', function() {
+    browser.get('http://localhost:4200/crear-videojuego');
+    browser.sleep(3000);
+
+    element(by.name('nombre')).sendKeys('FIFA20');
+    element(by.name('genero')).sendKeys('Botero');
+    browser.sleep(2000);
+    element(by.name('fecha')).sendKeys('12-13-2019');
+    browser.sleep(2000);
+    element(by.name('precio')).sendKeys(300000);
+    element(by.name('autor')).sendKeys('Electronics');
+    element(by.id('registrar')).click();
+  });
+});
+
+describe('listarVideojuego', function() {
+  it('listarVideojuego', function() {
+    browser.get('http://localhost:4200/videojuegos');
+    expect(browser.getTitle()).toEqual('FrontendTiendaVideojuegos');
+    browser.sleep(3000);
+  });
+});
+
+describe('insertarReserva', function() {
+  it('insertarReserva', function() {
+    browser.get('http://localhost:4200/crear-reserva');
+    browser.sleep(3000);
+
+    element(by.name('idCliente')).sendKeys(1);
+    browser.sleep(2000);
+    element(by.name('idVideojuego')).sendKeys(1);
+    browser.sleep(2000);
+    element(by.id('registrar')).click();
+  });
+});
+
+describe('listarReservas', function() {
+  it('listarReservas', function() {
+    browser.get('http://localhost:4200/reservas');
+    expect(browser.getTitle()).toEqual('FrontendTiendaVideojuegos');
+    browser.sleep(3000);
   });
 });
